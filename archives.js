f5e1145c2dacbed6759e047ae5b9eeeac212f3e6
Maximmusse <darksaburos.com@gmail.com>
2021-09-08 03:14:53 +0100

    UPLOAD3
    I added the folder to github

---
+++ archives.js
@@ -0,0 +1,51 @@
+/*function benifunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"HOME/beni.html\" width=\"100%\" height=\"800vmin\" style=\"border-style: none;\"></iframe>"
+}
+function merefunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"HOME/mere.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+function martinfunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/martin.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+function ashleyfunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/ashley.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+function resticfunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/restic.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+function arnaudfunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/arnaud.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+function jeromefunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/jerome.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+function saturninfunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/saturnin.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+function elizabethfunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/elizabeth.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+function samarangefunction(){
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/samarange.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+/*
+function {
+    document.getElementById("informations").innerHTML="<iframe class=\"iframes\" src=\"Home/.html\" width=\"100%\" height=\"100%\" style=\"border-style: none;\"></iframe>"
+}
+
+}*/
+function xer () {
+    let a = 0
+ setInterval((function() {
+   for (let x = 0; x < 101; x++) {
+     /*document.getElementsByClassName("progress-bar")[0].style=`width : ${a}%`;
+     document.getElementById("progress_num").innerHTML=a+"%";*/
+     if(x==100&&a<100){
+       a+=1;
+     }
+     
+   }
+ }), 1000);
+  }
+  xer()
+
